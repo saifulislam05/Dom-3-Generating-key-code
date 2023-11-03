6f40938cf8940ad0022326fbedb274c1e55e6e64
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-03 18:56:09 +0530

    adde new feature storing the keys history

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const keyCodeDisplay = document.getElementById("key-code");
 
 const keySound = document.getElementById("key-sound");
 
+const history = document.getElementById("history");
+const pressedKeyWrapper = document.getElementById("pressed-keyWrapper");
+// Key History Array
+
+let keyHistory = [];
+
 document.addEventListener("keydown", (event) => {
   keySound.play();
 
@@ -32,7 +38,44 @@ document.addEventListener("keydown", (event) => {
 
   const modifierKeys = modifiers.join(" + ");
 
-  pressedKeyDisplay.textContent =
+  let pressedKey =
     modifierKeys.length > 0 ? `${modifierKeys} + ${event.key}` : event.key;
+  pressedKeyDisplay.textContent = pressedKey;
+   
   keyCodeDisplay.textContent = event.keyCode;
+
+  const timeStamp = new Date().toLocaleTimeString();
+
+  keyHistory.push({key:pressedKey,code:event.keyCode,time:timeStamp})
+
+  history.style.display = "block";
+  displayKeyHistory();
+
 });
+
+function displayKeyHistory (){
+  // Last key of keyHistory
+  const lastentry = keyHistory[keyHistory.length - 1];
+
+  // distracutes all the entry's keys
+
+  const { key, time } = lastentry;
+  // creating new entry wrapper for ui
+  const pressedKeyDiv = document.createElement("div");
+  pressedKeyDiv.className = "pressedKey";
+
+  const keyParagraph = document.createElement("p");
+  keyParagraph.className = "key";
+  keyParagraph.textContent = key; 
+
+  const timeSpan = document.createElement("span");
+  timeSpan.className = "time";
+  timeSpan.textContent = time; 
+
+  pressedKeyDiv.appendChild(keyParagraph);
+  pressedKeyDiv.appendChild(timeSpan);
+
+  pressedKeyWrapper.prepend(pressedKeyDiv);
+}
+
+
